e9446fdb4806d6eaaaf5c3de1c9aef45c1ae74b7
marcelo <mrg0809@gmail.com>
2023-12-05 23:31:31 -0600

    login and signup functions

--- gentlemansbet/login/views.py
+++ gentlemansbet/login/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -6,7 +8,32 @@ def HomePage(request):
     return render(request, 'home.html')
 
 def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Los password no son iguales")
+        
+        else:
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')
+
+        print(uname, email, pass1, pass2)
+
     return render(request, 'signup.html')
 
 def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            return redirect('homemembers')
+        else:
+            return HttpResponse('Usuario o password incorrecto')
     return render(request, 'login.html')
